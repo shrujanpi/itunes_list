9ade37e12492084053c2bbe1a94fc2962ab2add7
Shru <__this__@this-s-MacBook-Pro.local>
2019-10-22 15:59:59 -0500

    Album model added

---
+++ itunes_list/itunes_list/Model/Album.swift
@@ -0,0 +1,21 @@
+//
+//  Album.swift
+//  itunes_list
+//
+//  Created by __this__ on 10/22/19.
+//  Copyright © 2019 __this__. All rights reserved.
+//
+
+import Foundation
+
+struct Album {
+    var albumName: String?
+    var artistName: String?
+    var thumbnail: String?
+    
+    // details page data
+    var genre: String?
+    var releaseDate: String?
+    var copyrightInfo: String?
+    var iTunesLink: String?
+}
